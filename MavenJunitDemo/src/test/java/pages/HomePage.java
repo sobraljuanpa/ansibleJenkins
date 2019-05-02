c131b9bf889478d4c83f3b154e3498053297a54e
unknown <sobraljuanpa@gmail.com>
2019-05-02 11:30:02 -0300

    Fixed multiline string formatting

--- MavenJunitDemo/src/test/java/pages/HomePage.java
+++ MavenJunitDemo/src/test/java/pages/HomePage.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
-    
+
     WebDriver driver;
     By myAccountButton = By.cssSelector("#top-links > ul > li.dropdown > a > i");
-    By registerButton = By.cssSelector("#top-links > ul > li.dropdown.open > ul
-     > li:nth-child(1) > a");
-    By loginButton = By.cssSelector("#top-links > ul > li.dropdown.open > ul
-     > li:nth-child(2) > a");
+    By registerButton = By.cssSelector("#top-links > ul > li.dropdown.open " +
+        "> ul > li:nth-child(1) > a");
+    By loginButton = By.cssSelector("#top-links > ul > li.dropdown.open > ul" +
+        " > li:nth-child(2) > a");
 
     public void clickRegister()
     {
