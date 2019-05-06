2d116f62c4ad7a087fc62bfac65a0115a99ee4eb
unknown <sobraljuanpa@gmail.com>
2019-05-06 15:06:42 -0300

    Corrected overlook directory change for maven execution of tests and added email notification support

--- jenkins/pipelines/runSeleniumTests.groovy
+++ jenkins/pipelines/runSeleniumTests.groovy
@@ -11,6 +11,14 @@ node {
     }
 
     stage('Execute tests using maven') {
-        sh 'mvn test'
+        sh 'cd MavenJunitDemo'//me muevo al directorio con el POM.xml
+        sh 'mvn clean test'//ejecuto las pruebas usando maven
+    }
+    //mover los archivos al fileserver
+
+    //enviar notificacion por mail
+    stage('Send email notification') {
+        def commitInfo = sh 'git log --format=format:%s -1'
+        emailext attachLog: true, body: '', subject: 'Test results for ${commitInfo}', to: 'matias.fornara@abstracta.com.uy'
     }
 }
