e629b05925d935c4437cc10b0473a009789c1482
unknown <sobraljuanpa@gmail.com>
2019-05-07 14:22:20 -0300

    Created search test suite

--- MavenJunitDemo/src/test/java/pages/HomePage.java
+++ MavenJunitDemo/src/test/java/pages/HomePage.java
@@ -9,7 +9,10 @@ public class HomePage {
     By myAccountButton = By.cssSelector("#top-links > ul > li.dropdown > a > i");
     By registerButton = By.cssSelector("#top-links > ul > li.dropdown.open > ul > li:nth-child(1) > a");
     By loginButton = By.cssSelector("#top-links > ul > li.dropdown.open > ul > li:nth-child(2) > a");
-
+    By searchBar = By.xpath("//input[@value='search']");
+    By searchButton = By.cssSelector("#search > span > button > i");
+    By searchResult = By.cssSelector('#content > div:nth-child(8) > div > div > div.image > a > img');
+    
     public HomePage(WebDriver webDriver)
     {
         driver = webDriver;
@@ -25,5 +28,14 @@ public class HomePage {
     {
         driver.findElement(myAccountButton).click();
         driver.findElement(loginButton).click();
+    }
+    
+    public void search(String productName) {
+        driver.findElement(searchBar).sendKeys(productName);
+        driver.findElement(searchButton).click();
+    }
+
+    public void selectFirstSearchResult() {
+        driver.findElement(searchResult).click();
     }
 }
