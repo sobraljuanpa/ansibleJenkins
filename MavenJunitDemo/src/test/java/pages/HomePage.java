65e159f96e206b56f8dfba7388383bebea00ea07
unknown <sobraljuanpa@gmail.com>
2019-05-08 12:03:55 -0300

    Added test for adding products to wishlist

--- MavenJunitDemo/src/test/java/pages/HomePage.java
+++ MavenJunitDemo/src/test/java/pages/HomePage.java
@@ -12,7 +12,9 @@ public class HomePage {
     By searchBar = By.cssSelector("#search > input");
     By searchButton = By.cssSelector("#search > span > button > i");
     By searchResult = By.cssSelector("#content > div:nth-child(8) > div > div > div.image > a > img");
-    
+    By addButton = By.cssSelector("#content > div:nth-child(1) > div.col-sm-4 > div.btn-group > button:nth-child(1)");
+    By successPopup = By.cssSelector("body > div:nth-child(4) > div.alert.alert-success");
+
     public HomePage(WebDriver webDriver)
     {
         driver = webDriver;
@@ -38,4 +40,13 @@ public class HomePage {
     public void selectFirstSearchResult() {
         driver.findElement(searchResult).click();
     }
+
+    public void addToWishlist() {
+        driver.findElement(addButton).click();
+    }
+
+    public bool addedConfirmed() {
+        return driver.findElement(successPopup).isVisible();
+    }
+    
 }
